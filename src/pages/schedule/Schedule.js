e8599a3ee0a83fafc885e7851bd8822f3f482755
kimchae0121 <169031995+kimchae0121@users.noreply.github.com>
2025-08-10 22:27:15 +0900

    초기 세팅 수정

---
+++ src/pages/schedule/Schedule.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./schedule.module.css";
+
+function Schedule() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Schedule Page</h1>
+      {/* 여기에 컴포넌트 배치 */}
+    </div>
+  );
+}
+
+export default Schedule;
