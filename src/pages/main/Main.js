a51441310578bd93161f7f8e1a47e1efd92db922
kimchae0121 <169031995+kimchae0121@users.noreply.github.com>
2025-08-11 10:56:06 +0900

    [FEAT] 메인 페이지 추가

---
+++ src/pages/main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./main.module.css";
+
+function Main() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Main Page</h1>
+      {/* 여기에 컴포넌트 배치 */}
+    </div>
+  );
+}
+
+export default Main;
