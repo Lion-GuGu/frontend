5c726bed64c9235bae36d36d4fc3ab672b7b6ad4
kimchae0121 <169031995+kimchae0121@users.noreply.github.com>
2025-08-22 20:50:49 +0900

    [feat] 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/main/MainPage.jsx";
 import Schedule from "./pages/schedule/Schedule.jsx";
+import ScheduleMonth from "./pages/scheduleMonth/ScheduleMonth.jsx";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/schedule" element={<Schedule />} />
+        <Route path="/scheduleMonth" element={<ScheduleMonth />} />
       </Routes>
     </BrowserRouter>
   );
